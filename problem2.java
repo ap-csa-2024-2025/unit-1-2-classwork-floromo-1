f031a534c43533b7ab367b28acdc03d805627a45
floromo <flormmmp@gmail.com>
2024-09-04 09:57:13 -0700

    completed #1-3

--- problem2.java
+++ problem2.java
@@ -1,1 +1,12 @@
 // Create your class here; don't forget the main method
+public class problem2 {
+    public static void main (String[] args) {
+        String name1, name2, name3, name4;
+        name1 = "Sandy ";
+        name2 = "David ";
+        name3 = "Elaine ";
+        name4 = "Jesse ";
+
+        System.out.println(name4 + name2 + name3 + name1);
+    }
+}
